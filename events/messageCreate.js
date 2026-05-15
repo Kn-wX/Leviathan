2cb14b28ecef976ae2ac39d762ddcb02e504b4e0
Knowx <swagchintu09@gmail.com>
2026-05-15 14:46:36 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -269,7 +269,7 @@ module.exports = async (client) => {
                                 `Hey ${message.author},\nMy Prefix here is: \`${message.guild.prefix}\`\nServer Id: \`${message.guild.id}\`\n\nType \`${message.guild.prefix}\`**help** To Get The Command List.`
                             )
                             .setFooter({
-                                text: `Made by r3tract0`,
+                                text: `Made by Knowx and Kopeekool studio`,
                                 iconURL: user.displayAvatarURL({
                                     dynamic: true
                                 })
