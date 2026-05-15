5c2d5f4de2e7c2aa9a2bbc43210d5f1996c97277
Knowx <swagchintu09@gmail.com>
2026-05-15 14:47:47 +0530

    Update help.js

--- commands/information/help.js
+++ commands/information/help.js
@@ -89,7 +89,7 @@ module.exports = {
                 `${client.emoji.giveaway} \`:\` **Giveaway**`
             )
             .setFooter({
-                text: `Developed By r3tract0 and Kopeekool studio`,
+                text: `Developed By Knowx and Kopeekool studio`,
                 iconURL: developerUser?.displayAvatarURL({ dynamic: true })
             });
 
